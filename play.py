5b24e1de9b706e99bb20f32e532ca6431398b700
prowe <prowe@harbornet.com>
2021-10-28 16:24:07 -0700

    Modified play.py so that play_game can be called from the command line *or* from within the IDE by a module such as driver.py. This allows running from the IDE for cases where the code cannot yet be run from the command line (e.g. because Jericho cannot be found there)

--- play.py
+++ play.py
@@ -9,7 +9,7 @@ from agent import HumanAgent
 
 
 def play_game(agent: Agent, game_file: str, num_steps: int):
-    """ The main method that instantiates an agent and plays the specified game"""
+    """ The method that runs the game play. """
 
     # Create the environment
     env = FrotzEnv(game_file)
@@ -17,6 +17,8 @@ def play_game(agent: Agent, game_file: str, num_steps: int):
     # The history is a list of (observation, action) tuples
     history = []
 
+    # Get the initial observation and info
+    # info is a dictionary (i.e. hashmap) of {'moves': int, 'score':int}
     curr_obs, info = env.reset()
     done = False
 
@@ -24,7 +26,14 @@ def play_game(agent: Agent, game_file: str, num_steps: int):
     print("Initial Observation\n" + curr_obs)
 
     while num_steps > 0 and not done:
+        
+        # For each step of game play, the agent determines the next action
+        # based on env and the history of observations and actions
+        # env is the environment from Frotz
         action_to_take = agent.take_action(env, history)
+        
+        # Take the action, which produces a new observation and updated info
+        # done is true wh
         next_obs, _, done, info = env.step(action_to_take)
 
         history.append((curr_obs, action_to_take))
@@ -43,26 +52,44 @@ def play_game(agent: Agent, game_file: str, num_steps: int):
         print(action)
 
 
+def main(agent, game_file, num_moves):
+    """ 
+    The main method that instantiates an agent and calls play_game for the
+    specified game.
+    """
+
+    
+    # Right now all you can create is a RandomAgent or a HumanAgent. 
+    # This will expand in the future.
+    if agent == 'random':
+        ai_agent = RandomAgent()
+    elif agent == 'human':
+        ai_agent = HumanAgent()
+    else:
+        ai_agent = RandomAgent()
+
+    play_game(ai_agent, game_file, num_moves)
+    
+
 if __name__ == "__main__":
-    # Use a parser for the command line arguments
-    # num_moves -- The number of moves the agent should make
-    # agent -- Right now this is just 'random' but will expand as we make other agents
-    # game_file -- The full path to the game file
+    """
+    Read in command line arguments and begin the game play.
+    Use a parser for the command line arguments
+    num_moves -- The number of moves the agent should make
+    agent -- Right now this is just 'random' but will expand as we make 
+    other agents
+    game_file -- The full path to the game file
+    """
     parser = argparse.ArgumentParser(
         description='Runs an AI agent on a specified game')
 
+    # Get the argements that were read in from the command line
     parser.add_argument(
         'num_moves', type=int, help='Number of moves for the agent to make')
     parser.add_argument('agent', help='[random|human]')
     parser.add_argument('game_file', help='Full pathname for game')
     args = parser.parse_args()
+    
+    # Call the main code to set up the agent and start the game
+    main(args.agent, args.game_file, args.num_moves)
 
-    # Right now all you can create is a RandomAgent. This will expand in the future
-    if args.agent == 'random':
-        ai_agent = RandomAgent()
-    elif args.agent == 'human':
-        ai_agent = HumanAgent()
-    else:
-        ai_agent = RandomAgent()
-
-    play_game(ai_agent, args.game_file, args.num_moves)
