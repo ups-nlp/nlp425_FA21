0d821b5bec03e17e4569363c002ca3b8152ef755
Eli Arao <earao@hotmail.com>
2021-10-28 17:22:28 -0700

    Added more Comments and description to general outline

--- mcts_agent.py
+++ mcts_agent.py
@@ -1,9 +1,19 @@
 
 # Expand From the given node
-def expandNode(parent, action):
-    bestAction; # Choose best action
-    newNode = node()
+def expandNode(parent):
+    # Get ready to store the selected action
+    action = ""
+
+    # Find the best action
+    for action in node.actions: ## Node store its possible actions? or rather actions as method parameter
+        action = "" ## Somehow select what action to do
+
+    # Make a new node
+    newNode = node() ## Node knows what action led to this state? as argument for constructor?
+
     ## Add newNode as child node of parent
+
+    # Return the newly created node to-be-explored
     return newNode
 
 
