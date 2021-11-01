e040221951f77ff4d53aa044b19c52d452322d9b
Anna Van Boven <annavanboven@Annas-MacBook-Air-5.local>
2021-11-01 12:51:21 -0700

    added default policy to mcts_agent file, as well as reward function

--- mcts_agent.py
+++ mcts_agent.py
@@ -1,6 +1,6 @@
 
 from math import inf, sqrt, log2
-
+import random
 
 
 
@@ -22,6 +22,53 @@ def expandNode(parent):
     # Return the newly created node to-be-explored
     return newNode
 
+def defaultPolicy(state):
+    """
+    The defaultPolicy represents a simulated exploration of the tree from
+    the passed-in node to a terminal state. 
+    """
+    explored = [] #track all explored actions from the current state
+    curr = state
+    x = 0
+    DEPTH = 10 #change this value depending on how far down the tree to search
+    while x<= 10:
+        """
+        INIT. DEFAULT POLICY: explore a random action from the list of available actions.
+        Once an action is explored, remove from the available actions list
+        """
+        index = random.randint(0, len(curr.actions))
+        act = curr.actions[index]
+        explored.append(act)
+        curr.actions.remove(act)
+
+        #create a new state resulting from taking action in current state
+        """
+        ERROR: to expand on any state, won't we need the possible actions for that state?
+        And isn't this determined by the feedback from the game in response fo our action,
+         which we won't have access to before actually taking that action.
+         Currently, I am sending forward the list of remaining unexplored actions, but this is not valid
+        """
+        curr = Node(curr, act, curr.actions)
+        x+=1
+    #store the reward received by reaching terminal state
+    rew = reward(curr)
+
+    #reset simulation to current state (add back actions)
+    while curr != state:
+        temp = curr.parent
+        curr = curr.remove()
+        temp.actions.append(explored[len(explored)-1])
+        explored.remove(explored[len(explored)-1])
+        curr = temp
+    #return the reward
+    return rew
+
+def reward(state):
+    """
+    The reward method will calculate the reward of any state given a reward function we will determine later.
+    Currently, it just returns the amount of possible actions remaining.
+    """
+    return len(state.actions)
 
 #######################
 
