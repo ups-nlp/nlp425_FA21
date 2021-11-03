a58079cc4b9ac60abcf29635e3ddaa3e1845dc8b
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-02 18:36:54 -0700

    Created the init() constructor in the MCTS agent class to generate the tree and altered the MCTS agent's take_action method to determine the ideal action to take at the current state using the tree

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ Agents for playing text-based games
 """
 
 import random
+import mcts_agent
 from jericho import FrotzEnv
 
 
@@ -34,8 +35,34 @@ class HumanAgent(Agent):
 class MonteAgent(Agent):
     """"Monte Carlo Search Tree Player"""
 
+
+    def __init__(self, env: FrotzEnv):
+        # create root node with the initial state
+        self.root = mcts_agent.Node(None, None)
+        # create a pointer node to use to traverse the tree later
+        self.current = self.root
+        # This constant balances tree exploration with exploitation of ideal nodes
+        self.exploreConst = 2
+
+        # train the tree using the Monte Carlo Search Algorithm
+        count = 0
+        # for now, we will only generate a tree with 101 nodes
+        while(count < 100):
+            # Create a new node on the tree
+            newNode = mcts_agent.treePolicy(self.root, env, self.exploreConst)
+            # Determine the simulated value of the new node
+            delta = mcts_agent.defaultPolicy(newNode, env)
+            # Propogate the simulated value back up the tree
+            mcts_agent.backUp(newNode, delta)
+
+            # reset the state of the game when done with one simulation 
+            env.reset()
+            count += 1
+
+    
     def take_action(self, env: FrotzEnv, history: list) -> str:
         """Takes in the history and returns the next action to take"""
         print("Action: ")
-        return input()
+        self.current = mcts_agent.bestChild(self.current, self.exploreConst)
+        return self.current.getPrevAction()
 
