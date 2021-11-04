9538c93394d876761fdfec014df4bf37eb14c29c
Benjamin Williams <--get>
2021-11-03 17:52:17 -0700

    Benepar stuff.

--- actions.py
+++ actions.py
@@ -1,9 +1,18 @@
 from jericho import FrotzEnv
 import numpy as np
 import spacy 
+import benepar
 game_file = "/mnt/c/users/meani/Documents/cs425/GAME/z-machine-games-master/jericho-game-suite/zork1.z5"
 
+
+# Some init stuff for NLP and Benepar
 nlp = spacy.load('en_core_web_sm')
+benepar.download('benepar_en3')
+
+if spacy.__version__.startswith('2'):
+	nlp.add_pipe(benepar.BeneparComponent("benepar_en3"))
+else:
+	nlp.add_pipe("benepar", config={'model': 'benepar_en3'})
 
 #def get_valid_actions():
 	# This must output a list
@@ -11,12 +20,11 @@ nlp = spacy.load('en_core_web_sm')
 
 def get_directions(input_string):
 	doc = nlp(input_string) #run information from game through the nlp pipeline
-
-
-	
-
+	text = [token.text for token in doc]
+	pos  = [token.pos_ for token in doc]
 
 	# Using input of enviornment, determine directional words
+	print(doc._.parse_string)
 
 
 if __name__ == "__main__" :
