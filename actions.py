8eb81fa17bf9888a88e274e8ac9bff78e4371088
Benjamin Williams <--get>
2021-11-05 02:09:55 -0700

    git Updated get_directions to contain a rudimentary directions dictionary, though at this point there is no intelligent parsing of direcitons. Function will digest input paragraphs sentence-by-sentence into NLP pipeline to generate a parse tree, but no further processing yet.

--- actions.py
+++ actions.py
@@ -7,7 +7,8 @@ game_file = "/mnt/c/users/meani/Documents/cs425/GAME/z-machine-games-master/jeri
 
 # Some init stuff for NLP and Benepar
 nlp = spacy.load('en_core_web_sm')
-benepar.download('benepar_en3')
+# Uncomment this if you need to download, but it lags my interpreter 
+# benepar.download('benepar_en3')
 
 if spacy.__version__.startswith('2'):
 	nlp.add_pipe(benepar.BeneparComponent("benepar_en3"))
@@ -19,18 +20,41 @@ else:
 	# Brayan is working on this
 
 def get_directions(input_string):
-	doc = nlp(input_string) #run information from game through the nlp pipeline
-	text = [token.text for token in doc]
-	pos  = [token.pos_ for token in doc]
+	#Extremely rudimentary dictionary containing direction info 
+	directionDict = {
+		"north" : {'north', 'front', 'ahead'},
+		"south" : {'south', 'behind', 'back'},
+		"east"  : {'east', 'right'},
+		"west"  : {'west', 'left'}
+	}
 
-	# Using input of enviornment, determine directional words
-	print(doc._.parse_string)
+	working_strings = input_string.split('\n')
+
+	for s in working_strings:
+		doc = nlp(s) #run information from game through the nlp pipeline
+		text = [token.text for token in doc]
+		pos  = [token.pos_ for token in doc]
+		sent = list(doc.sents)[0]
+
+		print("POS: ", pos)
+		print("TEXT: ", text)
+
+		# Using input of enviornment, determine directional words
+		parsed = sent._.parse_string
+
+		# Plan for later:
+		# Read parse tree to understand relationship between direction and object
+		# E.x. differentiate "West of house" means going east goes to house
+		# v.s. "to the north" means going north follows that path 
+
 
 
 if __name__ == "__main__" :
 	#Things to be run from commandline 
 	env = FrotzEnv(game_file)
-	info = env.reset()
-	text = info[0]
+	info = env.reset()[0]
 
-	get_directions(text)
+	#cleans out the copyright info for pipeline
+	info = info.split('\n', maxsplit = 4)[-1].strip()
+
+	get_directions(info)
