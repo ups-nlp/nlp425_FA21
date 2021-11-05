324c77ec0f06428a5758d759359ce8afb41d4f1c
Brayan Rodriguez <brayanrodriguez@TH257G-00651LR0.pugetsound.edu>
2021-11-05 12:55:23 -0700

    cleaned up file

--- actions.py
+++ actions.py
@@ -1,4 +1,4 @@
-#@author brayancodes
+#@author brayancodes, ben
 # Imports from my jupyter notebook.
 from collections import Counter
 import math
@@ -61,6 +61,7 @@ if __name__ == "__main__" :
 	info = info.split('\n', maxsplit = 4)[-1].strip()
 
 	get_directions(info)
+    get_nouns(info)
 
 # Test Input from Zork 1
 input_1 = "You are facing the south side of a white house. There is no door here, and all the windows are boarded."
@@ -69,51 +70,26 @@ input_3 = "You are in the kitchen of the white house. A table seems to have been
 input_4 = "You are in the living room. There is a doorway to the east, a wooden door with strange gothic lettering to the west, which appears to be nailed shut, a trophy case, and a large oriental rug in the center of the room."
 input_5 = "With a great effort, the rug is moved to one side of the room, revealing the dusty cover of a closed trap door."
 
-# Prompt for Input
-# user_input = input("Enter 1 - 5 to choose a different sentences to parse from Zork 1: ")
-# if user_input == 1:
-#     desired_input = input_1
-# elif user_input == 2:
-#     desired_input = input_2
-# elif user_input == 3:
-#     desired_input = input_3
-# elif user_input == 4:
-#     desired_input = input_4
-# else:
-#     desired_input = input_5
-# print()
-# print("You have chosen this sentence:")
-# print(desired_input)
-# print()
+# ===== get_nouns ======
+def get_nouns(input):
 
-# Parse the desired input & print
-#doc = nlp(desired_input)
-doc = nlp(input_1)
-sentences = (doc.sents)
+    # Giving spacey the sentence
+    doc = nlp(input)
+    sentences = (doc.sents)
 
-# The Main Method
+    text = [token.text for token in doc] # Words
+    pos = [token.pos_ for token in doc] # Pos tags
 
+    words_to_get = [] # This is storing the indexes for the words which are nouns.
+    counter = 0
+    for word in pos:# This is finding the indexes for the nouns.
+        if word == 'NOUN':
+            words_to_get.append(counter)
+        counter += 1
 
-# The Standard Function for the Final Project
-# def get_valid_actions( obs)
+    words_we_wanted = [] # This is storing the actual nouns
+    for index in words_to_get: # Pulling the noun from its list using the found index.
+        words_we_wanted.append(text[index])
 
-
-# ===== get_nouns <- Brayan ======
-text = [token.text for token in doc] # Words
-pos = [token.pos_ for token in doc] # Pos tags
-
-words_to_get = [] # This is storing the indexes for the words which are nouns.
-counter = 0
-for word in pos:# This is finding the indexes for the nouns.
-    if word == 'NOUN':
-        words_to_get.append(counter)
-    counter += 1
-
-words_we_wanted = [] # This is storing the actual nouns
-for index in words_to_get: # Pulling the noun from its list using the found index.
-    words_we_wanted.append(text[index])
-
-print()
-print(words_we_wanted)
-
-# get_directions <- Ben
+    print()
+    print(words_we_wanted)
