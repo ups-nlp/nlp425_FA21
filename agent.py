f778c46888c7627b19672fe2f42d98ecf8fa0f74
Anna Van Boven <annavanboven@Annas-MacBook-Air-5.local>
2021-11-08 13:21:16 -0800

    moved the tree expansion into take_action

--- agent.py
+++ agent.py
@@ -47,32 +47,6 @@ class MonteAgent(Agent):
         # This constant balances tree exploration with exploitation of ideal nodes
         self.explore_const = 2
 
-        # The total number of nodes to create in the tree
-        max_nodes = 2000
-
-        # The length of each monte carlo simulation
-        simulation_length = 10
-
-        # Train the agent using the Monte Carlo Search Algorithm
-        count = 0
-        done = False
-        
-        while(count < max_nodes and not done):
-            print(count)
-            # Create a new node on the tree
-            new_node = mcts_agent.tree_policy(self.root, env, num_steps, self.explore_const)
-            # Determine the simulated value of the new node
-            delta = mcts_agent.default_policy(new_node, env, simulation_length)
-            # Propogate the simulated value back up the tree
-            mcts_agent.backup(new_node, delta)
-
-            if env.victory():
-                done = True
-            # reset the state of the game when done with one simulation
-            env.reset()
-            count += 1
-
-        #self.root.print(0)
 
 
     def take_action(self, env: FrotzEnv, history: list) -> str:
@@ -81,6 +55,35 @@ class MonteAgent(Agent):
         print(env.get_valid_actions())
         for child in self.current.children:
             print(child.get_prev_action(), ", count:", child.visited, ", value:", child.sim_value, ", terminal:", child.terminal)
+
+        #
+        # Train the agent using the Monte Carlo Search Algorithm
+        #
+
+        # The length of each monte carlo simulation
+        simulation_length = 10
+
+        # Maximum number of nodes to generate in the tree each time a move is made
+        max_nodes = 2000
+
+        #current number of generated nodes
+        count = 0
+        
+        #current state of the game. Return to this state each time generating a new node
+        self.currState = env.get_state()
+        while(count <= max_nodes):
+            #print(count)
+            # Create a new node on the tree
+            new_node = mcts_agent.tree_policy(self.root, env, self.explore_const)
+            # Determine the simulated value of the new node
+            delta = mcts_agent.default_policy(new_node, env, simulation_length)
+            # Propogate the simulated value back up the tree
+            mcts_agent.backup(new_node, delta)
+            # reset the state of the game when done with one simulation
+            env = env.set_state(self.currState)
+            count += 1
+
+
         self.current = mcts_agent.select_action(self.current, self.explore_const)
         print(self.current)
         return self.current.get_prev_action()
