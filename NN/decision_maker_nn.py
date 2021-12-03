927d71924dd6003d0daf2a2b967d53c89be3dedf
ericmarkewitz <61162496+ericmarkewitz@users.noreply.github.com>
2021-12-03 12:38:20 -0800

    Made changes to bad spelling in dep_agent. Also added a new directory to host the NN files.

---
+++ NN/decision_maker_nn.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 3 12:19:30 2021
+
+@author: Eric Markewitz
+"""
+
+#Decisionmaker NEURAL NET
+trainingInputs = []
+trainingOutputs = []
+
+#file containing the observation, action, and module of that action from the walkthrough
+dm_training_data = open("../data/dm_training_data.txt")
+for line in dm_training_data:
+    list = line.split(',')
+    observation = list[0]
+    obs_vect = self.create_vect(observation)
+
+    action = list[1]
+    module = list[2]
+    module = re.sub('\n', '', module)
+
+    trainingInputs.append(obs_vect)
+    trainingOutputs.append(module)
+
+
+np_input = np.array(trainingInputs)
+np_output = np.array(trainingOutputs)
+
+
+
+train_obs, test_obs, train_labels, test_labels = train_test_split(np_input, np_output, test_size = 0.2, random_state =1)
+
+#Hyperparameters
+epochs = 20
+batch_size = 32
+hidden_lyr1_nodes = 64
+hidden_lyr2_nodes = 64
+learning_rate = 0.005
+input_size = 50
+output_size = 4
+
+# Build the model
+model = Sequential([
+  Dense(hidden_lyr1_nodes, activation='relu', input_shape=(input_size,)),
+  Dense(hidden_lyr2_nodes, activation='relu'),
+  Dense(output_size, activation='softmax'),
+])
+
+# Compile the model.
+model.compile(
+  optimizer=Adam(lr=learning_rate),
+  loss='categorical_crossentropy',
+  metrics=['accuracy'],
+)
+
+# Train the model. The use of to_categorical converts the indices to the
+# actions to one-hot vectors
+history = model.fit(train_obs,
+                    to_categorical(train_labels),
+                    verbose = 0,
+                    validation_split = 0.2, # split data in 80/20 sets
+                    epochs=epochs,
+                    batch_size=batch_size)
+
+
+# Plot the accuracy of the model as it trains
+plt.plot(history.history['accuracy'])
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+
+# Evaluate the model.
+# output: accuracy is 0.425 on test set
+model.evaluate(test_obs, to_categorical(test_labels))
--- dep_agent.py
+++ dep_agent.py
@@ -40,7 +40,6 @@ from operator import truediv
 from agent import Agent
 
 
-
 class DEPagent(Agent):
     """Agent created by Danielle, Eric, and Penny."""
 
@@ -69,76 +68,10 @@ class DEPagent(Agent):
 
         self.vocab_vectors, self.word2id = self.embed_vocab()
 
-        #Decisionmaker NEURAL NET
-        trainingInputs = []
-        trainingOutputs = []
+        #Find the NN files that will save weights to a file
+        #create_dm_nn(self.vocab_vectors, self.word2id)
 
-        #file containing the observation, action, and module of that action from the walkthrough
-        dm_training_data = open("./data/dm_training_data.txt")
-        for line in dm_training_data:
-            list = line.split(',')
-            observation = list[0]
-            obs_vect = self.create_vect(observation)
-
-            action = list[1]
-            module = list[2]
-            module = re.sub('\n', '', module)
-
-            trainingInputs.append(obs_vect)
-            trainingOutputs.append(module)
-
-
-        #np_input = np.aray(trainingInputs)
-        #np_output = np.array(trainingOutputs)
-
-
-        '''
-        train_obs, test_obs, train_labels, test_labels = train_test_split(np_input, np_output, test_size = 0.2, random_state =1)
-
-        #Hyperparameters
-        epochs = 20
-        batch_size = 32
-        hidden_lyr1_nodes = 64
-        hidden_lyr2_nodes = 64
-        learning_rate = 0.005
-        input_size = 50
-        output_size = 4
-
-        # Build the model
-        model = Sequential([
-          Dense(hidden_lyr1_nodes, activation='relu', input_shape=(input_size,)),
-          Dense(hidden_lyr2_nodes, activation='relu'),
-          Dense(output_size, activation='softmax'),
-        ])
-
-        # Compile the model.
-        model.compile(
-          optimizer=Adam(lr=learning_rate),
-          loss='categorical_crossentropy',
-          metrics=['accuracy'],
-        )
-
-        # Train the model. The use of to_categorical converts the indices to the
-        # actions to one-hot vectors
-        history = model.fit(train_obs,
-                            to_categorical(train_labels),
-                            verbose = 0,
-                            validation_split = 0.2, # split data in 80/20 sets
-                            epochs=epochs,
-                            batch_size=batch_size)
-
-
-        # Plot the accuracy of the model as it trains
-        plt.plot(history.history['accuracy'])
-        plt.xlabel('epochs')
-        plt.ylabel('accuracy')
-
-        # Evaluate the model.
-        # output: accuracy is 0.425 on test set
-        model.evaluate(test_obs, to_categorical(test_labels))
-        '''
-
-        # set model for sentance transformers
+        # set model for sentence transformers
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
 
     def hoarder(self, env:FrotzEnv, valid_actions:list, history:list) -> str:
@@ -193,7 +126,7 @@ class DEPagent(Agent):
         @param valid_actions
         @param history
 
-        The mover will take some set of valid move actions, cross that will
+        The mover will take some set of valid move actions, cross that with
         other known move actions and will pick one by prioritizing new
         directions
 
