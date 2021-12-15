0a4cf4d6e004a302b3bee3153b99859ffc501af1
prowe <prowe@harbornet.com>
2021-12-15 11:11:20 -0800

    reduce probability of chosing NN action in EE module

--- dep_agent.py
+++ dep_agent.py
@@ -192,7 +192,7 @@ class DEPagent(Agent):
         encoded_obs = self.pcaEncoder.encode(np.array([curr_obs]))
 
         # Run the neural network to choose an action
-        predict = self.ee_model.predict(encoded_obs)
+        predict = 0.1 * self.ee_model.predict(encoded_obs)
 
         # Add these probabilities to the list
         prob += predict[0]
